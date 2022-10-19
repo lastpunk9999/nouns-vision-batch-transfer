c266fb582c2fd394e8631c327593065b6b9c2ced
punk9999 <punk9999@protonmail.com>
2022-10-18 21:25:36 -0400

    deploy script

--- script/Deploy.sol
+++ script/Deploy.sol
@@ -2,11 +2,13 @@
 pragma solidity ^0.8.13;
 
 import "forge-std/Script.sol";
+import "../src/NounsVisionBatchTransfer.sol";
 
-contract CounterScript is Script {
+contract Deploy is Script {
     function setUp() public {}
 
     function run() public {
         vm.broadcast();
+        new NounsVisionBatchTransfer();
     }
 }
